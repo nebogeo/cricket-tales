b1e64328e1c176f147d8486dc67e3c4878f29e89
Dave Griffiths <dave@fo.am>
2014-09-14 10:09:47 +0100

    popcorn experiments

--- media/js/base.js
+++ media/js/base.js
@@ -3,3 +3,47 @@ $(document).ready(function(){
         axis:"y",
     });
 });
+
+function video_setup(image) {
+    // Create a popcorn instance by calling Popcorn("#id-of-my-video")
+    document.addEventListener("DOMContentLoaded", function () {
+
+        var pop = Popcorn("#ourvideo");
+        console.log(pop);
+
+        pop.image({
+        // seconds
+            start: 2,
+            // seconds
+            end: 15,
+            href: "",
+            src: image,
+            text: "DRUMBEAT",
+            target: "footnotediv"
+        });
+
+        pop.code({
+            start: 2.2,
+            end: 20.7,
+            onStart: function() {
+              }
+        });
+
+        $("#time").draggable({
+            axis:"x",
+        });
+
+
+        pop.on("timeupdate",
+               function() {
+                   var percentage = Math.floor((100 / pop.duration()) *
+                                               pop.currentTime());
+                   $("#time").css({left: percentage+"%"});
+               }
+              );
+
+        // play the video right away
+        pop.play();
+    },false);
+
+};
