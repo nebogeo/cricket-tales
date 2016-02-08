4c8e37cb04bed0575534c816bfee692ce94772e1
dave griffiths <dave@fo.am>
2016-02-08 14:51:32 +0000

    fix

--- crickets/views.py
+++ crickets/views.py
@@ -50,8 +50,8 @@ def map(request):
                 t = PlayerBurrowScore.objects.filter(player=request.user,burrow=burrow)
                 if t: player_score = t[0].movies_finished
                 highest_score = 0
-                t = PlayerBurrowScore.objects.filter(burrow=burrow).order_by('movies_finished')[0]
-                if t: highest_score = t.movies_finished
+                t = PlayerBurrowScore.objects.filter(burrow=burrow).order_by('movies_finished')
+                if t: highest_score = t[0].movies_finished
                 burrow.videos_to_view = highest_score - player_score
 
 
