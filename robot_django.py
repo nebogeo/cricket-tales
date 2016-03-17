b2ad63c6195f46c18d0f62e3bf752563a58d055f
dave griffiths <dave@fo.am>
2016-03-17 12:15:31 +0000

    trying to get positions fixed

--- robot_django.py
+++ robot_django.py
@@ -242,8 +242,8 @@ def shuffle_burrows(empties):
         p = robot.maths.find_new_location(random.choice(empties),poslist)
         poslist.append(p)
         # fix earlier dave lat/lon confusion
-        burrow.pos_x = p[0]*2.0
-        burrow.pos_y = p[1]/2.0
+        burrow.pos_x = p[0]/2.0
+        burrow.pos_y = p[1]*2.0
         burrow.save()
 
 def process_random_video(instance_name):
