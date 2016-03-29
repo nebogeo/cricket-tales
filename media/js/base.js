9c5403fce06c4c1825b070a6a89f0bda1f8c6c5a
dave griffiths <dave@fo.am>
2016-03-29 20:07:33 +0100

    no cricket button fix, and some layout fixes: #130

--- media/js/base.js
+++ media/js/base.js
@@ -220,29 +220,29 @@ function enter_id(t) {
 
 function update_infotext() {
     // need to make the font smaller than the usual to fit anything in
-    $('.page_title').css("font-size","120%");
+    //$('.page_title').css("font-size","0.01vw;");
     switch(state) {
         case "wait-cricket":
-            $('.page_title').html('1. Click on the cricket if you can see it, or click <button id="no_cricket">No Cricket</button>');
+            $('.menu_title').html('1. Click on the cricket if you can see it, or click <button id="no_cricket">No Cricket</button>');
             break;
         case "wait-burrow":
-            $('.page_title').html('2. Click on the middle of the burrow to begin the video');
+            $('.menu_title').html('2. Click on the middle of the burrow to begin the video');
             break;
         case "movie-playing":
-            $('.page_title').html('3. Tag cricket behaviours and ID as the video plays');
+            $('.menu_title').html('3. Tag cricket behaviours and ID as the video plays');
             break;
         case "wait-cricket-end":
-            $('.page_title').html('4. Click on the cricket if you can see it, or click <button id="no_cricket_end">No cricket</button>');
+            $('.menu_title').html('4. Click on the cricket if you can see it, or click <button id="no_cricket_end">No cricket</button>');
             break;
         case "movie-end":
-            $('.page_title').html('Well done! Video complete');
+            $('.menu_title').html('Well done! Video complete');
             break;
         // whyyy
         case "no-cricket-end":
-            $('.page_title').html('No cricket clicked [no cricket seen?]');
+            $('.menu_title').html('No cricket clicked [no cricket seen?]');
             break;
         default:
-            $('.page_title').html('I have no idea, you broke me');
+            $('.menu_title').html('I have no idea, you broke me');
 
     }
 }
